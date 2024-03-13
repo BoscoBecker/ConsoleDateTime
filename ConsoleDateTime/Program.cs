c830eeb2bc520672dad3a546551f9a1732c2826d
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-03-12 21:41:16 -0300

    Add project files.

---
+++ ConsoleDateTime/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Data = DateTime.Now;
+            Console.WriteLine(Data);
+        }
+
+
+
+    }
+
+}
